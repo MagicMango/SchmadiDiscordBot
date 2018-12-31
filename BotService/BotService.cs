9806cbcdab477722223ff295822b58c69df31596
Stefan Sievert <stefan.sievert@stud-informatik.de>
2018-12-31 12:41:42 +0100

    Init Commit

---
+++ BotService/BotService.cs
@@ -0,0 +1,30 @@
+﻿using DiscordBotCore.Handler;
+using System.Diagnostics;
+using System.ServiceProcess;
+using System.Threading.Tasks;
+
+namespace BotService
+{
+    public partial class BotService : ServiceBase
+    {
+        public BotService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            Debugger.Launch();
+            ChannelHandler s = new ChannelHandler();
+            Task.Run(async () => {
+                await s.RunBotAsync();
+            })
+            .ConfigureAwait(false);
+            
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
---
+++ TestConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿using DiscordBotCore.Handler;
+using System;
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChannelHandler h = new ChannelHandler();
+            var t = h.GetBot;
+            t.Wait();
+            Console.ReadKey();
+        }
+    }
+}
