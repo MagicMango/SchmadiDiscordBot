4ac93667528c77c50d043c64cf08813e74696fd8
Stefan Sievert <stefan.sievert@stud-informatik.de>
2019-01-01 15:24:50 +0100

    Wisdom befehle gewrapped

---
+++ DiscordBotCore/Util/Configuration.cs
@@ -0,0 +1,20 @@
+﻿namespace DiscordBotCore.Util
+{
+    public static class Configuration
+    {
+        public static string GetStringSetting(string setting)
+        {
+            return System.Configuration.ConfigurationManager.AppSettings[setting];
+        }
+
+        public static int GetIntSetting(string setting)
+        {
+            return int.Parse(System.Configuration.ConfigurationManager.AppSettings[setting]);
+        }
+
+        public static bool GetBoolSetting(string setting)
+        {
+            return bool.Parse(System.Configuration.ConfigurationManager.AppSettings[setting]);
+        }
+    }
+}
